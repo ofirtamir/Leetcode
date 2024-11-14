c6e5d033cd0dadaf253e33126844a756b85ce76f
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-11-14 17:43:03 +0200

    Time: 403 ms (96.44%), Space: 19.7 MB (92.72%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -33,5 +33,6 @@ class Solution:
                     r -= 1
                     while nums[l]==nums[l-1] and l< r :
                         l= l+1 
-
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
         return res
