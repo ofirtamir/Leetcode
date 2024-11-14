747d74e2e13bae3c1cdcee10d65bd6c3c2fb247c
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-11-14 23:06:18 +0200

    Time: 19 ms (53.94%), Space: 16.7 MB (69.06%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,20 +1,30 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        res =0
-        dic = collections.defaultdict(set)
-        if len(s)==1:
-            return 1
-        l =0
-        r = 1
-        while r < len(s):
-            dic[l].add(s[l])
-            if s[r] not in dic[l]:
-                dic[l].add(s[r])
-                r += 1
-            else:
+        # res =0
+        # dic = collections.defaultdict(set)
+        # if len(s)==1:
+        #     return 1
+        # l =0
+        # r = 1
+        # while r < len(s):
+        #     dic[l].add(s[l])
+        #     if s[r] not in dic[l]:
+        #         dic[l].add(s[r])
+        #         r += 1
+        #     else:
+        #         l+=1
+        #         r=l+1
+        # for a,i in enumerate(dic.values()):
+        #     print(i)
+        #     res =max(res, len(i))
+        # return res
+        r, l =0, 0
+        ss =set()
+        res=0
+        for r in range(len(s)):
+            while s[r] in ss:
+                ss.remove(s[l])
                 l+=1
-                r=l+1
-        for a,i in enumerate(dic.values()):
-            print(i)
-            res =max(res, len(i))
+            ss.add(s[r])
+            res=max(res, r-l +1)
         return res
