67fd85db942f0c97315fdb01b6b2d4d07a44af51
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-11-16 14:17:26 +0200

    Time: 0 ms (100%), Space: 16.7 MB (14.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,21 +1,32 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        for i in range(len(s)):
-            if s[i] == '(' or s[i] == '{' or s[i] == '[':
-                stack.append(s[i])
+        # stack = []
+        # for i in range(len(s)):
+        #     if s[i] == '(' or s[i] == '{' or s[i] == '[':
+        #         stack.append(s[i])
+        #     else:
+        #         if len(stack) ==0:
+        #             return False
+        #         top = stack.pop()
+        #         if top == '(' and s[i] != ')':
+        #             return False
+        #         if top == '[' and s[i] != ']':
+        #             return False
+        #         if top == '{' and s[i] != '}':
+        #             return False
+        #         if top == None:
+        #             return False
+        # if len(stack)>0:
+        #     return False
+        # return True
+        stack= []
+        dic = {"}":"{", "]":"[",")":"("}
+        for i in s:
+            if i in dic:
+                if stack and stack[-1]== dic[i]:
+                    stack.pop()
+                else:
+                    return False
             else:
-                if len(stack) ==0:
-                    return False
-                top = stack.pop()
-                if top == '(' and s[i] != ')':
-                    return False
-                if top == '[' and s[i] != ']':
-                    return False
-                if top == '{' and s[i] != '}':
-                    return False
-                if top == None:
-                    return False
-        if len(stack)>0:
-            return False
-        return True
+                stack.append(i)
+        return True if not stack else False
