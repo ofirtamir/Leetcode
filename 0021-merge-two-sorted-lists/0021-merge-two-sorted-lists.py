9b125e2ffb96f18f8af1a44e456dec51732887ba
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-11-20 21:41:07 +0200

    Time: 3 ms (14.69%), Space: 16.9 MB (9.88%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,31 +5,46 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        if list1.val <= list2.val:
-            res =list1
-            curr1=list1.next
-            curr2=list2
-        else:
-            res = list2
-            curr2= list2.next
-            curr1=list1
-        current =res
+        # if not list1:
+        #     return list2
+        # if not list2:
+        #     return list1
+        # if list1.val <= list2.val:
+        #     res =list1
+        #     curr1=list1.next
+        #     curr2=list2
+        # else:
+        #     res = list2
+        #     curr2= list2.next
+        #     curr1=list1
+        # current =res
 
-        while curr1 and curr2:
-            if curr1.val <= curr2.val:
-                current.next=curr1
-                curr1=curr1.next
-            else:
-                current.next=curr2
-                curr2=curr2.next
-            current = current.next
+        # while curr1 and curr2:
+        #     if curr1.val <= curr2.val:
+        #         current.next=curr1
+        #         curr1=curr1.next
+        #     else:
+        #         current.next=curr2
+        #         curr2=curr2.next
+        #     current = current.next
             
-        if curr1:
-            current.next=curr1
-        if curr2:
-            current.next=curr2
-        return res
+        # if curr1:
+        #     current.next=curr1
+        # if curr2:
+        #     current.next=curr2
+        # return res
+        dammy = ListNode()
+        node =dammy
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next=list1
+                list1=list1.next
+            else:
+                node.next =list2
+                list2= list2.next
+            node= node.next
+        if list1:
+            node.next= list1
+        elif list2:
+            node.next =list2
+        return dammy.next
