0412fd238dd9e186d378ea753e962cc6eaa7f326
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-11-23 09:49:04 +0200

    Time: 226 ms (85.6%), Space: 19 MB (55.4%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,4 +1,28 @@
 class Solution:
+    def dfs(self,grid,r,c):
+        if r>= len(grid) or r<0 or c>= len(grid[0]) or c< 0 or grid[r][c]=="0":
+            return
+        grid[r][c] ="0"
+        self.dfs(grid, r-1,c)
+        self.dfs(grid, r+1,c)
+        self.dfs(grid,r,c+1)
+        self.dfs(grid,r,c-1)
+    def numIslands(self, grid:List[List[str]]):
+        if not grid:
+            return 0
+        res = 0
+        rows, cols = len(grid) ,len(grid[0])
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1":
+                    res+=1
+                    self.dfs(grid,r,c)
+        return res
+
+
+
+
+
     # def numIslands(self, grid: List[List[str]]) -> int:
     #     if not grid:
     #         return 0
@@ -25,22 +49,23 @@ class Solution:
     #                 res+= 1
 
     #     return res
-    def dfs(self, grid, i, j):
-        m, n = len(grid), len(grid[0])
-        if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] == '0':
-            return
-        grid[i][j] = '0'
-        self.dfs(grid, i + 1, j)
-        self.dfs(grid, i - 1, j)
-        self.dfs(grid, i, j + 1)
-        self.dfs(grid, i, j - 1)
 
-    def numIslands(self, grid: List[List[str]]) -> int:
-        m, n = len(grid), len(grid[0])
-        count = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1':
-                    count += 1
-                    self.dfs(grid, i, j)
-        return count
+    # def dfs(self, grid, i, j):
+    #     m, n = len(grid), len(grid[0])
+    #     if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] == '0':
+    #         return
+    #     grid[i][j] = '0'
+    #     self.dfs(grid, i + 1, j)
+    #     self.dfs(grid, i - 1, j)
+    #     self.dfs(grid, i, j + 1)
+    #     self.dfs(grid, i, j - 1)
+
+    # def numIslands(self, grid: List[List[str]]) -> int:
+    #     m, n = len(grid), len(grid[0])
+    #     count = 0
+    #     for i in range(m):
+    #         for j in range(n):
+    #             if grid[i][j] == '1':
+    #                 count += 1
+    #                 self.dfs(grid, i, j)
+    #     return count
