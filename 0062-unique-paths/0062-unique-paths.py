7bc2e7b64f057820ce1efa61fdf48c3e07fbde05
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-11-23 13:54:12 +0200

    Time: 4 ms (8.56%), Space: 16.7 MB (21.49%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        mat = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        mat[m-1][n-1] = 1
+        for r in range(m-1, -1, -1):
+            for c in range(n-1, -1, -1):
+                if r== m-1 and c == n-1:
+                    continue
+                mat[r][c] = mat[r+1][c] + mat[r][c+1]
+
+        return mat[0][0]
+        
