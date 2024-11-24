2daa807bcffe6eb7631d94286a805ec8aca68ca7
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-11-24 22:27:57 +0200

    Time: 0 ms (100%), Space: 18.6 MB (95.87%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,5 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        x =Counter(nums)
-        return x.most_common()[-1][0]
+        # x =Counter(nums)
+        # return x.most_common()[-1][0]
+        res= 0
+        for n in nums:
+            res = n ^ res
+        return res
         
