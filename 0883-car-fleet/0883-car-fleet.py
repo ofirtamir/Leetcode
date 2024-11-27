fa6b64ff7842fda97e6d1ab536f8df866bec24d5
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-11-27 19:05:10 +0200

    Time: 285 ms (7.23%), Space: 37 MB (67.6%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.py
@@ -0,0 +1,12 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars=[[p,s] for p,s in zip(position, speed)]
+        cars.sort()
+        stack = []
+        for x in range(len(cars)-1,-1,-1):
+            if not stack: 
+                stack.append(cars[x])
+            elif (target - stack[-1][0])/ (stack[-1][1]) < ((target-cars[x][0])/ cars[x][1]):
+                stack.append(cars[x])
+            
+        return len(stack)
