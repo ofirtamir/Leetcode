6e9676912e7643491cef5f78c0e0e05a5d588bd7
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-12-01 14:30:16 +0200

    Time: 0 ms (100%), Space: 17.4 MB (5.85%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,21 +7,28 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         
-        def bfs(p, q):
-            if not p and not q:
-                return True
-            q1 = deque([p])
-            q2= deque([q])
-            while q1 and q2:
-                node1 = q1.popleft()
-                node2 = q2.popleft()
-                if not node1 and not node2:
-                    continue
-                if not node1 or not node2 or node1.val!=node2.val:
-                    return False
-                q1.append(node1.left)
-                q1.append(node1.right)
-                q2.append(node2.left)
-                q2.append(node2.right)
+        # def bfs(p, q):
+        #     if not p and not q:
+        #         return True
+        #     q1 = deque([p])
+        #     q2= deque([q])
+        #     while q1 and q2:
+        #         node1 = q1.popleft()
+        #         node2 = q2.popleft()
+        #         if not node1 and not node2:
+        #             continue
+        #         if not node1 or not node2 or node1.val!=node2.val:
+        #             return False
+        #         q1.append(node1.left)
+        #         q1.append(node1.right)
+        #         q2.append(node2.left)
+        #         q2.append(node2.right)
+        #     return True
+        # return bfs(p,q)
+
+        if not p and not q:
             return True
-        return bfs(p,q)
+        if p and q and p.val== q.val:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False 
