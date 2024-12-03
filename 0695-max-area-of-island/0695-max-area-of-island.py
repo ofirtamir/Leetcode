d6f9fcf322f5c607bb57f58008c9707874dc2ed5
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-12-03 16:10:34 +0200

    Time: 15 ms (91.11%), Space: 17.5 MB (46.97%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,14 +1,15 @@
 class Solution:
-    def dfs(self,grid,r,c,n):
+    def dfs(self,grid,r,c):
         if r<0 or r >= len(grid) or c < 0 or c>=len(grid[0]) or grid[r][c]==0:
             return 0
         grid[r][c]=0
-        n+=1
-        n+=self.dfs(grid,r+1,c,0)
-        n+=self.dfs(grid,r-1,c,0)
-        n+=self.dfs(grid,r,c+1,0)
-        n+=self.dfs(grid,r,c-1,0)
-        return n
+        # n+=1
+        # n+=self.dfs(grid,r+1,c,0)
+        # n+=self.dfs(grid,r-1,c,0)
+        # n+=self.dfs(grid,r,c+1,0)
+        # n+=self.dfs(grid,r,c-1,0)
+        # return n
+        return 1+ self.dfs(grid,r+1,c) +self.dfs(grid,r-1,c)+ self.dfs(grid,r,c+1) +self.dfs(grid,r,c-1) 
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         if not grid:
             return 0
@@ -16,5 +17,5 @@ class Solution:
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c]==1:
-                    m= max(m, self.dfs(grid, r, c,0))
+                    m= max(m, self.dfs(grid, r, c))
         return m
