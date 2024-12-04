ce79fb44ece4ca5b329759fae25be0531f34d80c
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-12-04 10:59:16 +0200

    Time: 2 ms (94.65%), Space: 20.5 MB (51.2%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,17 +2,11 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals:
             return []
-        
-        # מיון הקטעים לפי נקודת ההתחלה
         intervals.sort(key=lambda x: x[0])
-        
-        merged = []
-        for interval in intervals:
-            # אם הרשימה ריקה או שאין חפיפה עם הקטע האחרון
-            if not merged or merged[-1][1] < interval[0]:
-                merged.append(interval)
+        res=[]
+        for inter in intervals:
+            if not res or inter[0]> res[-1][1]:
+                res.append(inter)
             else:
-                # מיזוג קטעים חופפים
-                merged[-1][1] = max(merged[-1][1], interval[1])
-        
-        return merged
+                res[-1][1]= max(res[-1][1],inter[1])
+        return res
