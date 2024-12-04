7b11d2ea24beb75268546f0de67f72e07d88fca2
ofir tamir <127851334+ofirtamir@users.noreply.github.com>
2024-12-04 13:38:42 +0200

    Time: 39 ms (81%), Space: 17.7 MB (6.52%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -4,28 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not subRoot:  # עץ ריק תמיד תת-עץ
+        if not subRoot:
             return True
-        if not root:  # אם root ריק אך subRoot לא, הוא לא יכול להיות תת-עץ
+        if not root:
+            return False
+        if self.sub(root,subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right,subRoot)
+    def sub(self,root, subRoot):
+        if not root and not subRoot:
+            return True
+        if not root or not subRoot:
             return False
         
-        # בדיקה אם העצים שווים מתחילתם
-        if self.isSameTree(root, subRoot):
-            return True
-        
-        # בדיקה בתת-העצים של root
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:  # שני העצים ריקים
-            return True
-        if not p or not q:  # אחד ריק והשני לא
+        if root.val ==subRoot.val:
+            return True and self.sub(root.left,subRoot.left) and self.sub(root.right,subRoot.right)
+        else:
             return False
-        if p.val != q.val:  # הערכים שונים
-            return False
+            
         
-        # בדיקה רקורסיבית עבור תת-העצים
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
